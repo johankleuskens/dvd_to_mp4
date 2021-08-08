d91b6e77e39221303db9e5faab4be48c831dcc68
johan <johan@melderse.nl>
2021-08-08 08:58:21 +0200

    Cheking for VIDEO_TS direcorty

--- convert.sh
+++ convert.sh
@@ -1,6 +1,16 @@
 pushd /mnt/hgfs
 find * -prune -type d | while IFS= read -r d; 
 do
-	echo "$d"
+	echo Stepping into "$d/"
+	pushd "$d/"
+	# Check if directory contains VIDEO_TS directory
+	subdirs=`find * -prune -type d`
+	echo $subdirs
+	if [[ "$subdirs" == *"VIDEO_TS"* ]]; then
+	echo "Found VIDEO_TS directory"
+		pushd VIDEO_TS
+		popd
+	fi
+	popd
 done
 popd
