d44c2cbbe94d918d8b052450686b0eeecb3ece81
johan <johan@melderse.nl>
2021-11-11 19:52:54 +0100

    DVD conversion is working

--- convert.sh
+++ convert.sh
@@ -1,7 +1,16 @@
 # (C) Johan Kleuskens, aug 2021
 #
 # https://gist.github.com/atais/552ee207a673901eddfc199fe79a4a40
+# HandBrakeCLI -i '0002_DVD 02_8mm'/ -o test.mp4   --main-feature -e x264 -q 20 --keep-display-aspect --loose-anamorphic --encoder-preset slow -b 2000
 #
+
+OP=" --keep-display-aspect"
+OP="$OP --loose-anamorphic"
+OP="$OP --encoder-preset slow"
+OP="$OP -b 2000"
+
+OUTPUT_DIR="/mnt/hgfs/SGM_mp4"
+
 pushd /mnt/hgfs
 find * -prune -type d | while IFS= read -r d; 
 do
@@ -14,19 +23,25 @@ do
 	if [[ "$subdirs" == *"VIDEO_TS"* ]]
 	then
 		echo "Found VIDEO_TS directory"
-		pushd VIDEO_TS
 		echo "Converting from DVD to mp4 in directory $d"	
+		HandBrakeCLI -i "VIDEO_TS" --main-feature -o "$OUTPUT_DIR/$d.mp4" "$OP"
 		popd
 	elif [[ "$files" == "" ]]
 	then
 		echo "Skipping $d because this directory is empty"
+		popd
 	elif [[ "$files" == *".mp4"* ]]
 	then
 		echo "Skipping $d because this directory already contains an mp4 file"
+		popd
 	elif [[ "$files" == *".VOB"* ]]
 	then
 		echo "Converting from DVD to mp4 in directory $d"
+		popd
+		mkdir "$OUTPUT_DIR/$d"
+		HandBrakeCLI -i "$d" -o "$OUTPUT_DIR/$d/$d.mp4" --main-feature -e x264 -q 20 "$OP"
+	else
+		popd
 	fi
-	popd
 done
 popd
