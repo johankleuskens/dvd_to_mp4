2cd602b754454977e2a75e3bbe42e535d91087bc
Johan Kleuskens <johan.kleuskens@phact.nl>
2021-11-15 08:35:45 +0100

    Optimized the flow

--- convert.sh
+++ convert.sh
@@ -4,18 +4,20 @@
 
 OP=" --keep-display-aspect"
 OP="$OP --loose-anamorphic"
-OP="$OP --encoder-preset slow"
+OP="$OP --encoder-preset veryslow"
 OP="$OP -b 2000"
-OP="$OP --stop-at duration:30"
+#OP="$OP --stop-at duration:30"
 #OP="$OP --title 0"				# Scan all titles only
 
 OUTPUT_DIR_NAME="SGM_mp4"
-OUTPUT_DIR="/mnt/hgfs/$OUTPUT_DIR_NAME"
+OUTPUT_DIR="/home/johan/$OUTPUT_DIR_NAME"
+
+INPUT_DIR="/media/johan/TOSHIBA EXT/FILMS-Geschiedenis-Melderslo"
 
 #SAVEIFS=$IFS		# Store seperator
 #IFS=$'\n'			# Make CR seperator
 
-pushd /mnt/hgfs
+pushd "$INPUT_DIR"
 for d in *;
 do
     if [[ -d "$d" && ! "$d" == "$OUTPUT_DIR_NAME" ]];
@@ -45,24 +47,25 @@ do
 					echo "Converting title $t from VIDEO_TS to mp4 in directory $d"	
 					HandBrakeCLI -i "$d/VIDEO_TS" -o "$OUTPUT_DIR/$d/$d-$t.mp4"  --title $t -e x264 -q 20 $OP
 				done
+				
+			# Check if directory contains VOB files
+			elif [[ "$files" == *".VOB"* ]];
+			then
+				#Read handbrake's stderr into variable
+				rawout=$(HandBrakeCLI -i "$d" -t 0 2>&1 >/dev/null)
+				#Parse the variable using grep to get the count
+				count=$(echo $rawout | grep -Eao "\\+ title [0-9]+:" | wc -l)
+				# Loop through all titles
+				for t in $(seq $count)
+				do
+					echo "Converting from title $t from DVD to mp4 in directory $d"
+					HandBrakeCLI -i "$d" -o "$OUTPUT_DIR/$d/$d-$t.mp4" --title $t -e x264 -q 20 $OP 
+				done
+				
 			# Check if directory is empty
 			elif [[ "$files" == "" ]];
 			then
 				echo "Skipping $d because this directory is empty"
-				
-			# Check if directory contains valid video files
-			elif [[ "$files" == *".avi"* || "$files" == *".MOV"* || "$files" == *".mpg"* || "$files" == *".MOD"* ]];
-			then
-				pushd "$d"
-				for f in *;
-				do
-					if [[ "$f" == *".avi"* || "$f" == *".MOV"* || "$f" == *".mpg"* || "$f" == *".MOD"* ]];
-					then
-						echo "Converting $f to mp4"
-						HandBrakeCLI -i "$f" -o "$OUTPUT_DIR/$d/$f.mp4" -e x264 -q 20 $OP
-					fi				
-				done
-				popd
 				
 			# Check if directory contains mp4 files
 			elif [[ "$files" == *".mp4"* ]];
@@ -78,21 +81,18 @@ do
 				done
 				popd
 				
-			# Check if directory contains VOB files
-			elif [[ "$files" == *".VOB"* ]];
-			then
-				#Read handbrake's stderr into variable
-				rawout=$(HandBrakeCLI -i "$d" -t 0 2>&1 >/dev/null)
-				#Parse the variable using grep to get the count
-				count=$(echo $rawout | grep -Eao "\\+ title [0-9]+:" | wc -l)
-				# Loop through all titles
-				for t in $(seq $count)
-				do
-					echo "Converting from title $t from DVD to mp4 in directory $d"
-					HandBrakeCLI -i "$d" -o "$OUTPUT_DIR/$d/$d-$t.mp4" --title $t -e x264 -q 20 $OP 
-				done
+			# Check if directory contains valid video files
 			else
-				echo "Unknown content in directory $d"
+				pushd "$d"
+				for f in *;
+				do
+					if [[ "$f" == *".avi"* || "$f" == *".MOV"* || "$f" == *".mpg"* || "$f" == *".MOD"* ]];
+					then
+						echo "Converting $f to mp4"
+						HandBrakeCLI -i "$f" -o "$OUTPUT_DIR/$d/$f.mp4" -e x264 -q 20 $OP
+					fi				
+				done
+				popd
 			fi
 		fi
 	fi
